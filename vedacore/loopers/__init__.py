3e7af11473654716dac07c5c6ad97cd24f06b64f
yangmin666 <33199285+yangmin666@users.noreply.github.com>
2022-11-02 15:21:59 +0800

    Update __init__.py

--- vedacore/loopers/__init__.py
+++ vedacore/loopers/__init__.py
@@ -1,4 +1,3 @@
 from .epoch_based_looper import EpochBasedLooper
-from .epoch_based_looper_normaltrain import EpochBasedLooper_normaltrain
 
-__all__ = ['EpochBasedLooper','EpochBasedLooper_normaltrain']
+__all__ = ['EpochBasedLooper']
