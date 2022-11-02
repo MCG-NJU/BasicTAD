38b4f9f008b5112675b748b357cb774b1855d968
yangmin666 <33199285+yangmin666@users.noreply.github.com>
2022-11-02 16:53:39 +0800

    Update __init__.py

--- vedatad/criteria/__init__.py
+++ vedatad/criteria/__init__.py
@@ -1,6 +1,5 @@
 from .builder import build_criterion
 from .segment_anchor_criterion import SegmentAnchorCriterion
-from .segment_anchor_criterion_anet import SegmentAnchorCriterion_anet
 from .fcos_criterion_batches_diou import FcosActFPNContextRegLossCriterion_batches_diou
 
-__all__ = ['SegmentAnchorCriterion_anet','SegmentAnchorCriterion','FcosActFPNContextRegLossCriterion_batches_diou','build_criterion']
+__all__ = ['SegmentAnchorCriterion','FcosActFPNContextRegLossCriterion_batches_diou','build_criterion']
