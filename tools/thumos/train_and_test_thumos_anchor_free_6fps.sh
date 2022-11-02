e4cc9f7f7e335f8786fc1e29fed255821464f5eb
yangmin666 <33199285+yangmin666@users.noreply.github.com>
2022-11-02 15:25:37 +0800

    Update train_and_test_thumos_anchor_free_6fps.sh

--- tools/thumos/train_and_test_thumos_anchor_free_6fps.sh
+++ tools/thumos/train_and_test_thumos_anchor_free_6fps.sh
@@ -1,8 +1,8 @@
-bash tools/dist_trainval.sh configs/trainval/basictad/thumos14_zhengli/basictad_slowonly_e700_thumos14_rgb_192win_anchor_free.py "0,1,2,3,4,5,6,7" 
+bash tools/dist_trainval.sh configs/trainval/basictad/thumos14/basictad_slowonly_e700_thumos14_rgb_192win_anchor_free.py "0,1,2,3,4,5,6,7" 
 
 for i in {1..12..1}
 do 
     a="$i"00_weights
     echo $a
-    CUDA_VISIBLE_DEVICES=4 python tools/thumos_zhengli/test_af.py --framerate 6 configs/trainval/basictad/thumos14_zhengli/basictad_slowonly_e700_thumos14_rgb_192win_anchor_free.py workdir/basictad_slowonly_e700_thumos14_rgb_192win_anchor_free/epoch_$a.pth
+    CUDA_VISIBLE_DEVICES=4 python tools/thumos/test_af.py --framerate 6 configs/trainval/basictad/thumos14/basictad_slowonly_e700_thumos14_rgb_192win_anchor_free.py workdir/basictad_slowonly_e700_thumos14_rgb_192win_anchor_free/epoch_$a.pth
 done
